83fdd82efa138057b7c6ad21c945a86d897e2655
Tatenda <tatenda.makunike@dotmodus.com>
2025-09-06 18:37:10 +0200

    fix: override the error in .d.ts

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { DefaultSession } from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+      role?: string;
+      isPremium?: boolean;
+    } & DefaultSession['user'];
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    role?: string;
+    isPremium?: boolean;
+  }
+}
